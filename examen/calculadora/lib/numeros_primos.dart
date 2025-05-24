175e30d338c84e3ae8989afc6ed9e711dd5c0517
willian0521 <carbajalwillian12@gmail.com>
2025-05-24 15:23:51 -0600

    PRIMOS

---
+++ examen/calculadora/lib/numeros_primos.dart
@@ -0,0 +1,25 @@
+import 'dart:math';
+
+void primos(int a, int b){
+    for(int n=a; n<=b; n++){
+        if(n>1){
+            if(n==2){
+                print(n);    
+            }else{
+                if(n%2!=0){
+                    int i=3;
+                    var prim=true;
+                    while(i<=sqrt(n) && prim){
+                        if((n%i)==0){
+                            prim=false;
+                        }
+                        i++;
+                    }
+                    if(prim==true){
+                        print(n);
+                    }
+                } 
+            }
+        }
+    }
+}
