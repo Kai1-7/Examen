40857632a599073872a9490e213b695396e6ab0f
Ka1-7 <kennethyanes81@gmail.com>
2025-05-24 14:57:46 -0600

    Test de Menu Helper

---
+++ examen/calculadora/lib/menu_helper.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+void mostrarMenuPrincipal() {
+  print('===== CALCULADORA MATEMATICA AVANZADA =====');
+  print('1. Suma               -> Ingrese dos numeros para sumarlos');
+  print('2. Resta              -> Ingrese dos numeros para restarlos');
+  print('3. Multiplicacion     -> Ingrese dos numeros para multiplicarlos');
+  print('4. División           -> Ingrese dos numeros y qud el segundo no debe ser cero');
+  print('5. Numeros Primos     -> Ingrese un rango para ver los primos en ese intervalo');
+  print('6. Pares e Impares    -> Ingrese un rango para ver numeros pares e impares');
+  print('7. Secuencia Fibonacci-> Ingrese cuantos terminos desea generar');
+  print('8. Areas Geometricas  -> Seleccione figura y proporcione dimensiones positivas');
+  print('9. Salir              -> Finaliza la aplicacion');
+  print('==========================================');
+  stdout.write('Seleccione una opcion (1-9): ');
+}
+
+void mostrarMenuAreas() {
+  print('--- CALCULO DE AREAS GEOMETRICAS ---');
+  print('1. Circulo     -> Ingrese el radio');
+  print('2. Rectangulo  -> Ingrese base y altura');
+  print('3. Triangulo   -> Ingrese base y altura');
+  print('4. Cuadrado    -> Ingrese el lado');
+  print('5. Volver al menu principal');
+  stdout.write('Seleccione una figura (1-5): ');
+}
